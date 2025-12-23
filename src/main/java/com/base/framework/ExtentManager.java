707dbcf35f6c8101d97636a7bd81d79af18e88b5
Atharva <atharvajoshibsl@gmail.com>
2025-12-23 22:48:02 +0530

    New Report for each execution

--- src/main/java/com/base/framework/ExtentManager.java
+++ src/main/java/com/base/framework/ExtentManager.java
@@ -1,21 +1,34 @@
 package com.base.framework;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
 
+    public static String RUN_ID;                 // <---- ADD THIS (public)
+    private static String reportFolderPath;
+    private static String reportFilePath;    
+    
     private static ExtentReports extent;
-    private static String reportPath;
     private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
     // Create and configure ExtentReports (only once)
     public synchronized static ExtentReports getExtentReports() {
         if (extent == null) {
-            reportPath = System.getProperty("user.dir") + "/Reports/ExtentReport.html";
+        	
+            // ----- CREATE PUBLIC RUN_ID -----
+            RUN_ID = "Run_" + DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss").format(LocalDateTime.now());
 
-            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            // ----- CREATE REPORT FOLDER -----
+            reportFolderPath = System.getProperty("user.dir")+ "/Reports/"+ RUN_ID;        	
+            
+            reportFilePath = reportFolderPath + "/ExtentReport.html";
+
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportFilePath);
             spark.config().setReportName("Automation Test Results");
             spark.config().setDocumentTitle("Test Execution Report");
 
@@ -28,10 +41,14 @@ public class ExtentManager {
         return extent;
     }
     
+    public static String getRunId() {
+        return RUN_ID;
+    }
+    
     public static String getReportPath()
     {
 //    	proxy python http server
-    	String extentUrl = "http://localhost:8081/Reports/ExtentReport.html";    	
+    	String extentUrl = "http://localhost:8081/Reports/"+ RUN_ID +"/ExtentReport.html";    	
     	return extentUrl;
     }
     
