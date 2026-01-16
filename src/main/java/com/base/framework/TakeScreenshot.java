ca463f4cfc92ae487f6de3ccb6c605c1a02d1d8e
Atharva <atharvajoshibsl@gmail.com>
2026-01-17 00:09:55 +0530

    final fix updates

--- src/main/java/com/base/framework/TakeScreenshot.java
+++ src/main/java/com/base/framework/TakeScreenshot.java
@@ -4,53 +4,32 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+
 import org.openqa.selenium.io.FileHandler;
 
 public class TakeScreenshot {
 
-    public static String capture(WebDriver driver, String fileName) throws IOException {
+    public static String capture(WebDriver driver, String name) throws IOException {
 
-        if (driver == null) {
-            throw new IllegalArgumentException("WebDriver is null. Cannot capture screenshot.");
-        }
-
-        // Jenkins build number or LOCAL
         String build = System.getenv("BUILD_NUMBER");
-        if (build == null) {
-            build = "LOCAL";
-        }
+        if (build == null) build = "LOCAL";
 
-        // FINAL screenshots folder (Jenkins-safe)
         File folder = new File(
                 System.getProperty("user.dir")
-                + File.separator + "Reports"
-                + File.separator + "build_" + build
-                + File.separator + ExtentManager.getRunId()
-                + File.separator + "screenshots"
+                + "/Reports/build_" + build + "/latest/screenshots"
         );
 
-        // Create directories safely
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
+        folder.mkdirs();
 
-        // UTC timestamp (CI + Grafana friendly)
-        String timestamp = Instant.now()
-                .toString()
-                .replaceAll("[:.T-]", "_");
-
-        String screenshotName = fileName + "_" + timestamp + ".png";
+        String ts = Instant.now().toString().replaceAll("[:.T-]", "_");
+        String file = name + "_" + ts + ".png";
 
         File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        File dest = new File(folder, screenshotName);
+        File dest = new File(folder, file);
 
         FileHandler.copy(src, dest);
 
-        // IMPORTANT: relative path from ExtentReport.html
-        return "screenshots/" + screenshotName;
- 
+        return "screenshots/" + file;
     }
 }
